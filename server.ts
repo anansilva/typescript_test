eeab8e0807b86bf1f614ae059bf291c6d4e13f8a
Ana Silva <ananunesdasilva@gmail.com>
2018-01-10 18:15:06 +0000

    show number of users per location

--- server.ts
+++ server.ts
@@ -29,10 +29,11 @@ const loc = argv['l'];
 
 if(userName == undefined && loc == undefined) {
   console.error('Please insert a valid input');
+  console.log('insert user: npm run test -- -u username')
+  console.log('list users in a location: npm run test -- -l location')
   process.exit();
 }
 
-// Results
 console.info('Connecting to the database:',
   `${options.user}@${options.host}:${options.port}/${options.database}`);
 
@@ -57,33 +58,64 @@ interface GithubUsers
 const pgp = pgPromise(pgpDefaultConfig);
 const db = pgp(options);
 
+
+// Insert user in the db
 if(userName !== undefined) {
 
-db.none('CREATE TABLE IF NOT EXISTS github_users (id BIGSERIAL, login TEXT UNIQUE, name TEXT, company TEXT, followers SERIAL, following SERIAl, location TEXT)')
-.then(() => request({
-  uri: `https://api.github.com/users/${userName}`,
-  headers: {
-        'User-Agent': 'Request-Promise'
-    },
-  json: true
-}))
-.then((data: GithubUsers) => db.one(
-  'INSERT INTO github_users (login, name, company, followers, following, location) VALUES ($[login], $[name], $[company], $[followers], $[following], $[location]) RETURNING id', data)
-).then(({id}) => console.log(id))
-.then(() => process.exit(0));
+  db.none('CREATE TABLE IF NOT EXISTS github_users (id BIGSERIAL, login TEXT UNIQUE, name TEXT, company TEXT, followers SERIAL, following SERIAl, location TEXT)')
+    .then(() => request({
+      uri: `https://api.github.com/users/${userName}`,
+      headers: {
+           'User-Agent': 'Request-Promise'
+      },
+      json: true
+    }))
+    .then((data: GithubUsers) => db.one(
+      'INSERT INTO github_users (login, name, company, followers, following, location) VALUES ($[login], $[name], $[company], $[followers], $[following], $[location]) RETURNING id', data)
+    ).then(({id}) => console.log(id))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    })
+    .then(() => {
+      locationStats();
+      });
 
 }
 
+// List users in a location
 if(loc !== undefined) {
 
   db.any(`SELECT login FROM github_users WHERE location LIKE'%${loc}%'`)
-     .then(data => {
-      console.log(`Users in ${loc}:`);
+    .then(data => {
+      console.info(`Users in ${loc}:`);
       data.forEach(element => {
         console.log(element.login)
       });
     })
-    .then(() => process.exit(0));
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    })
+    .then(() => {
+      locationStats();
+    });
 
+}
+
+// Show number of users per location
+const locationStats = () => {
+  db.any(`SELECT COUNT(login), location FROM github_users GROUP BY location ORDER BY COUNT(login) DESC`)
+    .then(data => {
+      console.info("Users per Location:");
+      data.forEach(result => {
+        console.log(`${result.location}  ${result.count}` )
+      })
+    })
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    })
+    .then(() => process.exit(0));
 }
 
